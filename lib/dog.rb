d6975667759bde0ff6c89d60b4ad6162bcab84d4
Matthew Bartl <mattbartl@gmail.com>
2019-04-03 02:37:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,15 @@ class Dog
     puts "Woof!"
   end
   def sit
-    puts "The dog is sitting."
+    puts "The dog is sitting"
   end
 end
 
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+  end
+end
